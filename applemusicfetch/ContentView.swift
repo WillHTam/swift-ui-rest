f794a99c60b09bf9083813e3206c5708375ae717
William T <william.tam18@gmail.com>
2019-12-19 19:19:58 +0800

    yeeha

--- applemusicfetch/ContentView.swift
+++ applemusicfetch/ContentView.swift
@@ -7,10 +7,84 @@
 //
 
 import SwiftUI
+import URLImage
+
+struct Response: Codable {
+    var results: [Result]
+}
+
+struct Result: Codable {
+    var trackId: Int
+    var trackName: String
+    var collectionName: String
+    var trackPrice: Double
+    var artworkUrl30: URL
+}
 
 struct ContentView: View {
+    @State var results = [Result]()
+    @State var searchTerm = ""
+    var disableForm: Bool {
+        searchTerm == ""
+    }
+    
     var body: some View {
-        Text("Hello, World!")
+        Form{
+            Section {
+                ZStack{
+                    TextField("Text Here", text: $searchTerm)
+                }
+                .frame(height: 20)
+            }
+            Section {
+                Button(action: {
+                    self.loadData()
+                }){
+                    Text("Search")
+                }
+                .disabled(disableForm)
+            }
+            Section {
+                List(results.prefix(12), id: \.trackId) {
+                    item in
+                    HStack {
+                        URLImage(item.artworkUrl30)
+                        VStack(alignment: .leading){
+                            Text(item.trackName)
+                                .font(.headline)
+                                .foregroundColor(.primary)
+                            Text(item.collectionName)
+                                .font(.subheadline)
+                                .foregroundColor(.secondary)
+                        }
+                    }
+                }
+            }
+        }
+    }
+    
+    func loadData() {
+        guard let url = URL(string: "https://itunes.apple.com/search?term=\(searchTerm.replacingOccurrences(of: " ", with: "+"))&entity=song") else {
+            print("Invalid URL")
+            return
+        }
+
+        let request = URLRequest(url: url)
+
+        URLSession.shared.dataTask(with: request) { data, response, error in
+            if let data = data {
+                if let decodedResponse = try? JSONDecoder().decode(Response.self, from: data) {
+                    DispatchQueue.main.async {
+                        // update our UI
+                        self.results = decodedResponse.results
+                    }
+                    // everything is good, so we can exit
+                    return
+                }
+            }
+            // if we're still here it means there was a problem
+            print("Fetch failed: \(error?.localizedDescription ?? "Unknown error")")
+        }.resume()
     }
 }
 
